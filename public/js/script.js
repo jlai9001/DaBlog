43c796e6a5dbb039cfd358b8db92918178aae7b4
Jonathan Lai <jlai9001@gmail.com>
2026-01-06 22:32:59 -0800

    empty search and back button fix

--- public/js/script.js
+++ public/js/script.js
@@ -4,12 +4,12 @@ document.addEventListener('DOMContentLoaded',function(){
     const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementById('searchInput');
     const searchClose = document.getElementById('searchClose');
+    const searchForm = document.querySelector('.search__form');
 
     for (let i = 0; i < allButtons.length; i++) {
         allButtons[i].addEventListener('click',function(){
             // null guard
             if (!searchBar || !searchInput) return;
-
             searchBar.style.visibility = 'visible';
             searchBar.classList.add('open');
             this.setAttribute('aria-expanded','true');
@@ -18,22 +18,43 @@ document.addEventListener('DOMContentLoaded',function(){
         });
     }
 
-    // null guard
+    // close button
     if (searchBar && searchInput && searchClose){
         searchClose.addEventListener('click',function(){
-            searchBar.style.visibility = 'hidden';
-            searchBar.classList.remove('open');
-            this.setAttribute('aria-expanded','true');
+        closeSearch();
+        });
+    }
+    // empty search interception
+    if (searchForm && searchInput) {
+        searchForm.addEventListener('submit', function (e) {
+        const value = searchInput.value.trim();
+
+        // Empty search = behave like close
+        if (!value) {
+            e.preventDefault();
+            closeSearch();
+        }
     });
+}
+
+    // close search
+    function closeSearch() {
+        if (!searchBar || !searchInput) return;
+        searchBar.style.visibility = 'hidden';
+        searchBar.classList.remove('open');
+        searchInput.value = '';
+        searchInput.blur();
+        allButtons.forEach(btn => {
+        btn.setAttribute('aria-expanded', 'false');
+        });
     }
 
     // register button
     const registerBtn = document.getElementById('register_btn');
-
-    if (!registerBtn) return;
-
-    registerBtn.addEventListener('click', () => {
-        window.location.href = '/admin/register';
-    });
+    if (registerBtn) {
+        registerBtn.addEventListener('click', () => {
+            window.location.href = '/admin/register';
+        });
+    }
 
 });
